0b9878cf9909f75e409accd2296ad67b3975f5b1
kos_ykt <kosvasl09@gamil.com>
2021-07-28 18:39:47 +0900

    make Toast visible

--- app/src/main/java/ru/kostrykt/gb_ak_hwapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/kostrykt/gb_ak_hwapp/viewmodel/MainViewModel.kt
@@ -3,13 +3,20 @@ package ru.kostrykt.gb_ak_hwapp.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
 class MainViewModel(private val liveDataToObserver: MutableLiveData<Any> = MutableLiveData())
     : ViewModel() {
 
     fun getData(): LiveData<Any>{
+        getDataFromLocalSource()
         return liveDataToObserver
     }
 
-
+    private fun getDataFromLocalSource() {
+        Thread{
+            sleep(2000)
+            liveDataToObserver.postValue(Any())
+        }.start()
+    }
 }
