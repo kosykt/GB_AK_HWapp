f130f31f408f18306dd07e7bc0ed5f3feca6d486
kos_ykt <kosvasl09@gamil.com>
2021-07-28 18:54:56 +0900

    ViewBinding for activity

--- app/src/main/java/ru/kostrykt/gb_ak_hwapp/view/MainActivity.kt
+++ app/src/main/java/ru/kostrykt/gb_ak_hwapp/view/MainActivity.kt
@@ -3,15 +3,20 @@ package ru.kostrykt.gb_ak_hwapp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.kostrykt.gb_ak_hwapp.R
+import ru.kostrykt.gb_ak_hwapp.databinding.MainActivityBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(binding.container.id, MainFragment.newInstance())
                 .commitNow()
         }
     }
