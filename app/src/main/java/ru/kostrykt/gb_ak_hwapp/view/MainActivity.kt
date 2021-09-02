0726f667dd1550ae70a680c8f42605817354cfa3
kos_ykt <kosvasl09@gamil.com>
2021-09-02 17:23:59 +0900

    HW11

--- app/src/main/java/ru/kostrykt/gb_ak_hwapp/view/MainActivity.kt
+++ app/src/main/java/ru/kostrykt/gb_ak_hwapp/view/MainActivity.kt
@@ -1,10 +1,19 @@
 package ru.kostrykt.gb_ak_hwapp.view
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import ru.kostrykt.gb_ak_hwapp.R
+import ru.kostrykt.gb_ak_hwapp.cloudmessage.CHANNEL_ID
 import ru.kostrykt.gb_ak_hwapp.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +29,35 @@ class MainActivity : AppCompatActivity() {
                 .replace(binding.container.id, MainFragment.newInstance())
                 .commitNow()
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            createNotificationChannel(notificationManager)
+        }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FIREBASEMSG", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            Log.d("FIREBASEMSG", token!!)
+        })
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channelName = "Channel name"
+        val descriptionText = "Channel description"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, channelName, importance).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
